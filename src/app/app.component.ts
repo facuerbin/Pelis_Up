8f05244303654b3adc35dd3ec3acccfe06a979ad
Facu Erbin <facuerbin@gmail.com>
2022-03-02 10:02:40 -0300

    Agrego cards y footer, corrijo detalles. Desafio terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { faArrowRight, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,21 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // Icons
   arrowRight = faArrowRight;
-  title = 'desafio';
+  searchIcon = faSearch;
+
+
+  isActive = 1;
+  sections = ["Todos", "Películas", "Series"];
+
+  buttonActive (id : number) {
+    this.isActive = id;
+    console.log("it works")
+    return true;
+  }
+
+  getActiveSection () {
+    return this.sections[this.isActive - 1];
+  }
 }
