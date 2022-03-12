fc2387af2cc2190e647336cab0bdee22bd75d551
Facu Erbin <facuerbin@gmail.com>
2022-03-12 03:08:38 -0300

    Agrego iconos faltantes al sitio

--- src/app/components/routes/login/login.component.ts
+++ src/app/components/routes/login/login.component.ts
@@ -1,23 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm!: FormGroup;
+  loginForm: FormGroup = this.formBuilder.group({
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(8)]]
+  });
+
+  envelopeIcon = faEnvelope;
+  keyIcon = faKey;
+
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      email: "",
-      password: ""
-    });
+    this.loginForm.valueChanges.subscribe((result) => {
+      console.log(result);
+      console.log(this.loginForm.controls["email"].errors)
+      console.log(this.loginForm.controls["password"].errors)
 
-    this.loginForm.valueChanges.subscribe(console.log);
+    })
+  }
+
+  isValid(field: string): boolean {
+    return this.loginForm.controls[field].errors !== null &&
+    (this.loginForm.controls[field].touched || this.loginForm.controls[field].dirty);
+  }
+
+  handleLogin() {
+    console.log("Handle login")
+
   }
 
 }
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
   apiKey : "8fb83c35cf1bf0d66a0a7a06e75482a9",
-  apiLanguage : "es-AR",
+  apiLanguage : "en-US",
   port : 4200
 }
